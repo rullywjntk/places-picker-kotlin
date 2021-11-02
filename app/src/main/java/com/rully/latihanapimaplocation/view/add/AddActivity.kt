d33004675666749ab49bbc0fae8623aad18bbfaf
Rully Wijiantoko <palmbiscuit08@gmail.com>
2021-11-02 16:10:38 +0700

    minor change packages

--- app/src/main/java/com/rully/latihanapimaplocation/view/add/AddActivity.kt
+++ app/src/main/java/com/rully/latihanapimaplocation/view/add/AddActivity.kt
@@ -1,4 +1,4 @@
-package com.rully.latihanapimaplocation
+package com.rully.latihanapimaplocation.view.add
 
 import android.Manifest
 import android.app.Activity
@@ -26,6 +26,7 @@ import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.rully.latihanapimaplocation.R
 import com.rully.latihanapimaplocation.databinding.ActivityAddBinding
 import java.io.File
 import java.io.FileOutputStream
