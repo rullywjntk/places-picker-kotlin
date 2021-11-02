bdecb57005f4733cbe0952178f97d213a864434b
Rully Wijiantoko <palmbiscuit08@gmail.com>
2021-11-02 15:28:43 +0700

    add select image from camera

--- app/src/main/java/com/rully/latihanapimaplocation/AddActivity.kt
+++ app/src/main/java/com/rully/latihanapimaplocation/AddActivity.kt
@@ -6,12 +6,13 @@ import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
+import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -21,6 +22,7 @@ import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.rully.latihanapimaplocation.databinding.ActivityAddBinding
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -35,10 +37,31 @@ class AddActivity : AppCompatActivity(), View.OnClickListener {
             if (it.resultCode == Activity.RESULT_OK) {
                 val data: Intent? = it.data
                 Glide.with(applicationContext)
-                    .load(data?.data)
+                    .load(data!!.data)
+                    .centerCrop()
                     .into(binding.ivLocation)
             }
         }
+
+    val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (result.data != null) {
+                    val data = result.data
+                    try {
+                        val bitmap = data?.extras?.get("data") as Bitmap
+                        Log.d("MyLogTag", "photo: $bitmap")
+                        Glide.with(applicationContext)
+                            .load(bitmap)
+                            .into(binding.ivLocation)
+
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -81,7 +104,7 @@ class AddActivity : AppCompatActivity(), View.OnClickListener {
                 imageDialog.setItems(imageDialogItems) { _, which ->
                     when (which) {
                         0 -> selectPhotoFromGallery()
-                        1 -> Toast.makeText(this, "coming soon", Toast.LENGTH_SHORT).show()
+                        1 -> selectPhotoFromCamera()
                     }
                 }.show()
             }
@@ -91,13 +114,36 @@ class AddActivity : AppCompatActivity(), View.OnClickListener {
     private fun selectPhotoFromGallery() {
         Dexter.withActivity(this).withPermissions(
             Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
         ).withListener(object : MultiplePermissionsListener {
             override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
                 if (report?.areAllPermissionsGranted() == true) {
                     val galleryIntent =
                         Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                     resultLauncher.launch(galleryIntent)
+                }
+            }
+
+            override fun onPermissionRationaleShouldBeShown(
+                permissions: MutableList<PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showRationalDialogPermissions()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun selectPhotoFromCamera() {
+        Dexter.withActivity(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report?.areAllPermissionsGranted() == true) {
+                    val cameraIntent =
+                        Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    cameraLauncher.launch(cameraIntent)
                 }
             }
 
